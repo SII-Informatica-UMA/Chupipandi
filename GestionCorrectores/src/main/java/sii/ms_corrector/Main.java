4f25f407acccc52cbf26d17d9487a46236f996f0
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2023-03-07 21:20:01 +0100

    Change package structure (GestionCorrectores)
    - New path: src/main/java/sii/ms_corrector

--- GestionCorrectores/src/main/java/sii/ms_corrector/Main.java
+++ GestionCorrectores/src/main/java/sii/ms_corrector/Main.java
@@ -1,18 +1,16 @@
-package JPA.Entidades_JPA;
+package sii.ms_corrector;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Main {
-
-	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA");
+public class Main 
+{
+    public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("ms_corrector");
 		EntityManager em = emf.createEntityManager();
-		
+        
 		em.close();
 		emf.close();
-
-	}
-
+    }
 }
--- GestionCorrectores/src/main/java/sii/ms_corrector/entities/Corrector.java
+++ GestionCorrectores/src/main/java/sii/ms_corrector/entities/Corrector.java
@@ -1,10 +1,10 @@
-package es.uma.informatica.sii.corrector.entities;
+package sii.ms_corrector.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Corrector {

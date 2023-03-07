1f046ff9db0608c569c53ad873326599b95977d8
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2023-03-07 21:04:57 +0100

    Change package structure (EvaluacionExamenes)
    - New path: src/main/java/sii/ms_evalexamenes

--- EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/entities/Corrector.java
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/entities/Corrector.java
@@ -1,14 +1,14 @@
-package es.uma.informatica.sii.evalexamenes.entities;
+package sii.ms_evalexamenes.entities;
 
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Corrector {
--- EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/entities/Examen.java
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/entities/Examen.java
@@ -1,10 +1,10 @@
-package es.uma.informatica.sii.evalexamenes.entities;
+package sii.ms_evalexamenes.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Examen {

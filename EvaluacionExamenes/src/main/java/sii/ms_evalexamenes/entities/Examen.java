493ce646b1541b0d21317ce805befda0fe408edc
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2023-03-14 21:34:28 +0100

    Fix imports

--- EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/entities/Examen.java
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/entities/Examen.java
@@ -2,12 +2,12 @@ package sii.ms_evalexamenes.entities;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 
 @Entity
 public class Examen {

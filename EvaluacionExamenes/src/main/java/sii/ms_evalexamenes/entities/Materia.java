5246c578bfc3335a9afef5560f45fc15de0c2806
MAngelGC <magc6102@uma.es>
2023-03-18 01:38:08 +0100

    Pasado el proyecto a Spring Boot

--- EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/entities/Materia.java
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/entities/Materia.java
@@ -2,57 +2,30 @@ package sii.ms_evalexamenes.entities;
 
 import java.util.ArrayList;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@EqualsAndHashCode(doNotUseGetters = true, onlyExplicitlyIncluded = true)
+@ToString(doNotUseGetters = true, exclude = {"correctores", "examenes"})
 @NoArgsConstructor
 public class Materia {
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) @EqualsAndHashCode.Include
     private Long id;
     @Column(nullable = false)
     private String nombre;
     private ArrayList<Long> correctores; // Una materia puede tener varios correctores (lista de ids de los correctores)
     @OneToMany(mappedBy = "materia")
     private ArrayList<Examen> examenes; // Una materia puede tener varios exámenes
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Materia other = (Materia) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-    
-    @Override
-    public String toString() {
-        return "Materia [id=" + id + ", nombre=" + nombre + "]";
-    }
 }

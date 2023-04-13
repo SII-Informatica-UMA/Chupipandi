d40fc4b62e73b57ad7b4ee051884d54915a4e0a4
Marcos <80154421+M4rdom@users.noreply.github.com>
2023-04-13 10:11:16 +0200

    Update Readme Corrector
    Se me ha olvidao comentar perdon

--- GestionCorrectores/runme.bat
+++ GestionCorrectores/runme.bat
@@ -5,6 +5,6 @@ start cmd.exe @cmd /k "java -cp h2-2.1.210.jar org.h2.tools.Server -ifNotExists"
 timeout /t 10
 :: CALL mvn exec:java -Dexec.mainClass="es.uma.informatica.jpa.demo.Main"
 :: CALL mvn exec:java -Dexec.mainClass="sii.ms_evalexamenes.Main"
-CALL mvn exec:java -Dexec.mainClass="sii.ms_corrector.Main"
+:: CALL mvn exec:java -Dexec.mainClass="sii.ms_corrector.Main"
 CALL java -jar ms_corrector-0.0.1-SNAPSHOT.jar
 PAUSE

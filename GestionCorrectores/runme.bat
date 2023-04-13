fe93cc8197b65c98dce69eb92d6117d331577d2e
Marcos <80154421+M4rdom@users.noreply.github.com>
2023-04-13 10:10:41 +0200

    Update runme Corrector

--- GestionCorrectores/runme.bat
+++ GestionCorrectores/runme.bat
@@ -6,4 +6,5 @@ timeout /t 10
 :: CALL mvn exec:java -Dexec.mainClass="es.uma.informatica.jpa.demo.Main"
 :: CALL mvn exec:java -Dexec.mainClass="sii.ms_evalexamenes.Main"
 CALL mvn exec:java -Dexec.mainClass="sii.ms_corrector.Main"
+CALL java -jar ms_corrector-0.0.1-SNAPSHOT.jar
 PAUSE

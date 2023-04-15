17d463c9ea0dab1daf519dcf96a1aa241bbedddd
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2023-04-15 21:45:42 +0200

    Update CorrectorDTO

--- GestionCorrectores/src/main/java/sii/ms_corrector/dtos/CorrectorDTO.java
+++ GestionCorrectores/src/main/java/sii/ms_corrector/dtos/CorrectorDTO.java
@@ -1,6 +1,6 @@
 package sii.ms_corrector.dtos;
 
-import java.util.List;
+import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,19 +19,39 @@ public class CorrectorDTO {
      * Puede ser que se simplifique el problema usando ModelMapper,
      * tanto para entidades locales como externas quiero pensar
      */
-    Long id;
-    Long identificadorUsuario;
-    String telefono;
-    int maximasCorrecciones;
-    List<MateriaEnConvocatoriaDTO> materias;
+    private Long id;
+    private Long identificadorUsuario;
+    private String telefono;
+    private int maximasCorrecciones;
+    // Set ya que son unicas
+    private Set<MateriaEnConvocatoriaDTO> materias;
 
     public static CorrectorDTO fromCorrector(Corrector corrector) {
         var dto = new CorrectorDTO();
         dto.setId(corrector.getId());
-        dto.setIdentificadorUsuario(null);  // de donde lo obtenemos?
-        dto.setTelefono(null);  // de donde lo obtenemos?
+        dto.setIdentificadorUsuario(corrector.getIdUsuario());  // de donde lo obtenemos?
+        dto.setTelefono(corrector.getTelefono());  // de donde lo obtenemos?
         dto.setMaximasCorrecciones(corrector.getMaximasCorrecciones());
-        dto.setMaterias(null);
-        return null;
+        // que son las materias en convocatoria?
+        // El id de la materia deberia ser siempre el mismo, puesto que cada corrector solo puede estar especializado
+        // en una materia.
+        // El id de la convocatoria entiendo yo que es lo unico que varia, ... habria que obtenerlo de la base de datos?
+        dto.setMaterias(Set.of(
+            MateriaEnConvocatoriaDTO.builder()
+            .idMateria(corrector.getMateriaEspecialista())
+            .idConvocatoria(0L).build()));
+        return dto;
+    }
+
+    public Corrector corrector() {
+        var correct = new Corrector();
+        correct.setId(id);
+        correct.setIdUsuario(identificadorUsuario);
+        correct.setTelefono(telefono);
+        // El id de la materia deberia ser siempre el mismo, puesto que cada corrector solo puede estar especializado
+        // un corrector siempre tendra un array con al menos una materia, no?
+        correct.setMateriaEspecialista(materias.stream().findFirst().get().getIdMateria());
+        correct.setMaximasCorrecciones(maximasCorrecciones);
+        return correct;
     }
 }

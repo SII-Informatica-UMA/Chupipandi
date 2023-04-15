8949519cff9c1f067dbab75b6aeda468290e7b96
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2023-04-15 21:46:11 +0200

    Update CorrectorNuevoDTO

--- GestionCorrectores/src/main/java/sii/ms_corrector/dtos/CorrectorNuevoDTO.java
+++ GestionCorrectores/src/main/java/sii/ms_corrector/dtos/CorrectorNuevoDTO.java
@@ -17,19 +17,32 @@ public class CorrectorNuevoDTO {
      * Puede ser que se simplifique el problema usando ModelMapper,
      * tanto para entidades locales como externas quiero pensar
      */
-    Long identificadorUsuario;
-    Long identificadorConvocatoria;
-    String telefono;
-    MateriaDTO materia;
-    int maximasCorrecciones;
+    private Long identificadorUsuario;
+    private Long identificadorConvocatoria;
+    private String telefono;
+    private MateriaDTO materia;
+    private int maximasCorrecciones;
 
+    // usaremos este metodo?
     public static CorrectorNuevoDTO fromCorrector(Corrector corrector) {
         var dto = new CorrectorNuevoDTO();
-        dto.setIdentificadorUsuario(null);              // de donde lo obtenemos?
-        dto.setIdentificadorConvocatoria(null);    // de donde lo obtenemos?
-        dto.setTelefono(null);                                      // de donde lo obtenemos?
-        dto.setMateria(null);                                        // de donde lo obtenemos?
+        dto.setIdentificadorUsuario(corrector.getIdUsuario());             // de donde lo obtenemos?
+        dto.setIdentificadorConvocatoria(corrector.getIdConvocatoria());    // de donde lo obtenemos?
+        dto.setTelefono(corrector.getTelefono());                                      // de donde lo obtenemos?
+        // de donde obtenemos el nombre de la materia? (capaz no usamos este metodo 'fromCorrector')
+        // (de momento no se usa)
+        dto.setMateria(MateriaDTO.builder().id(corrector.getMateriaEspecialista()).nombre("").build());
         dto.setMaximasCorrecciones(corrector.getMaximasCorrecciones());
-        return null;
+        return dto;
+    }
+
+    public Corrector corrector() {
+        var correct = new Corrector();
+        correct.setIdConvocatoria(identificadorConvocatoria);
+        correct.setIdUsuario(identificadorUsuario);
+        correct.setTelefono(telefono);
+        correct.setMateriaEspecialista(materia.getId());
+        correct.setMaximasCorrecciones(maximasCorrecciones);
+        return correct;
     }
 }

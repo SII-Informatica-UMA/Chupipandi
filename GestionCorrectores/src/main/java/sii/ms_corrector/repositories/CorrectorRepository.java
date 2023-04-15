f0a8e66cc0f90bf3fb008501e614ca2bd685f651
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2023-04-15 14:14:06 +0200

    Update Corrector repository

--- GestionCorrectores/src/main/java/sii/ms_corrector/repositories/CorrectorRepository.java
+++ GestionCorrectores/src/main/java/sii/ms_corrector/repositories/CorrectorRepository.java
@@ -1,9 +1,14 @@
 package sii.ms_corrector.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import sii.ms_corrector.entities.Corrector;
 
 public interface CorrectorRepository extends CrudRepository<Corrector, Long> {
+
+    Optional<List<Corrector>> findByIdConvocatoria(Long idConvocatoria);
     
 }

aa26cca5fb9fb68b4de56799936d0ca6bddf34ba
MAngelGC <magc6102@uma.es>
2023-04-17 18:16:13 +0200

    Creados DTO

---
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/dtos/AsignacionDTO.java
@@ -0,0 +1,27 @@
+package sii.ms_evalexamenes.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import sii.ms_evalexamenes.entities.Examen;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AsignacionDTO {
+
+    private Long idCorrector;
+    private Long idExamen;
+
+    public static AsignacionDTO fromExamen(Examen examen) {
+        var dto = new AsignacionDTO();
+        
+        dto.setIdCorrector(examen.getCorrectorId());
+        dto.setIdExamen(examen.getId());
+        return dto;
+    }
+}
---
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/dtos/NotificacionNotasDTO.java
@@ -0,0 +1,27 @@
+package sii.ms_evalexamenes.dtos;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NotificacionNotasDTO {
+    
+    private String plantillaAsunto;
+    private String plantillaCuerpo;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    private LocalDate programacionEnvio;
+    private List<String> medios;
+}

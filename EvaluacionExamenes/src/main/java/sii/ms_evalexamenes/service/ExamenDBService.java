90d579d9a76193a31fbb471a2afa3c196d858f63
Marcos <marcos105ftr@gmail.com>
2023-04-17 16:33:32 +0200

    Add Materia service methods

--- EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/service/ExamenDBService.java
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/service/ExamenDBService.java
@@ -6,11 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
 import sii.ms_evalexamenes.entities.Examen;
+import sii.ms_evalexamenes.entities.Materia;
 import sii.ms_evalexamenes.repositories.ExamenRepository;
+import sii.ms_evalexamenes.repositories.MateriaRepository;
 
 
 @Service
@@ -20,12 +23,18 @@ public class ExamenDBService {
 	
 	
 	private ExamenRepository examenRepository;
+	private MateriaRepository materiaRepository;
 
 
 	@Autowired
 	public ExamenDBService(ExamenRepository examenRepository) {
 		this.examenRepository = examenRepository;
 	}
+	
+	
+	
+	
+	//Service
 	
 	
 	public Optional<Examen> get_Examen_By_Id(Long id) {
@@ -41,8 +50,7 @@ public class ExamenDBService {
 		examenRepository.save(examen);
 		return examen.getId();
 	}
-	
-	
+		
 	public void delete_Examen(Long idExamen) throws Exception {
 		if (examenRepository.existsById(idExamen)) {
 			examenRepository.deleteById(idExamen);
@@ -59,6 +67,45 @@ public class ExamenDBService {
 			throw new Exception("Examen no Encontrado, no se ha modificado ningún Examen");
 		}
 	}
+	
+	
+	//Materia
+	
+	public Optional<Materia> get_Materia_By_Id(Long id) {
+		return materiaRepository.findById(id);
+	}
+	
+	public List<Materia> get_All_Materia() {
+		return materiaRepository.findAll();
+	}
+	
+	public Long add_Materia(Materia examen) {
+		examen.setId(null);
+		materiaRepository.save(examen);
+		return examen.getId();
+	}
+		
+	
+	
+	public void delete_Materia(Long idMateria) throws Exception {
+		if (materiaRepository.existsById(idMateria)) {
+			materiaRepository.deleteById(idMateria);
+		} else {
+			throw new Exception("Materia no Encontrada, no se ha eliminado ningúna Materia");
+		}
+	}
+	
+	
+	public void modify_Materia(Long idMateria, Materia materia) throws Exception {
+		if (materiaRepository.existsById(idMateria)) {
+			materiaRepository.save(materia);
+		} else {
+			throw new Exception("Materia no Encontrada, no se ha modificado ningúna Materia");
+		}
+	}
+	
+	
+	
 	
 	
 }

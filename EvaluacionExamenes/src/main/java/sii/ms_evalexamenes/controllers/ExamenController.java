07f6248a37c608445eda8c0cefa9c0b6d529b2d7
MAngelGC <magc6102@uma.es>
2023-04-19 19:55:58 +0200

    Asignación automática de exámenes a correctores.

--- EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/controllers/ExamenController.java
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/controllers/ExamenController.java
@@ -3,11 +3,12 @@ package sii.ms_evalexamenes.controllers;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import sii.ms_evalexamenes.dtos.AsignacionDTO;
@@ -26,6 +31,7 @@ import sii.ms_evalexamenes.dtos.ExamenDTO;
 import sii.ms_evalexamenes.entities.Examen;
 import sii.ms_evalexamenes.services.ExamenService;
 import sii.ms_evalexamenes.services.exceptions.AlreadyExistsException;
+import sii.ms_evalexamenes.services.exceptions.NotFoundException;
 import sii.ms_evalexamenes.services.exceptions.UnauthorizedAccessException;
 
 @RestController
@@ -38,10 +44,30 @@ public class ExamenController {
         this.service = service;
     }
 
+    private URI uri(String scheme, String host, int port, String ...paths) {
+        UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+        UriBuilder ub    = ubf.builder()
+                        .scheme(scheme)
+                        .host(host)
+                        .port(port);
+        for (String path : paths) {
+            ub = ub.path(path);
+        }
+        return ub.build();
+    }
+
+    private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.get(uri)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .build();
+        return peticion;
+    }
+
     @GetMapping("{id}")
-    public ResponseEntity<Examen> getExamen(@PathVariable Long id) {
-        Optional<Examen> examen = service.getExamenById(id);
-        return ResponseEntity.of(examen);
+    public ResponseEntity<ExamenDTO> getExamen(@PathVariable Long id) {
+        ExamenDTO examen = ExamenDTO.fromExamen(service.getExamenById(id).orElseThrow(NotFoundException::new));
+        return ResponseEntity.ok(examen);
     }
 
     @PutMapping("{id}")
@@ -54,7 +80,32 @@ public class ExamenController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> addExamen(@RequestBody ExamenDTO examen, UriComponentsBuilder builder) {
-        Long id = service.addExamen(examen.examen());
+        Examen examenNuevo = examen.examen();
+        var peticion = get("http", "localhost", 8081, "/correctores");
+    
+        
+
+        var respuesta = new RestTemplate().exchange(peticion, String.class);
+
+        JSONArray correctores = new JSONArray(respuesta.getBody());
+
+        for (int i = 0; i < correctores.length(); ++i) {
+            JSONObject corrector = correctores.getJSONObject(i);
+            if (service.getCorrectoresById(examen.getId()).get().size() + 1 <= corrector.getInt("maximasCorrecciones")) {
+                JSONArray materias = corrector.getJSONArray("materias");
+                for (int j = 0; j < materias.length(); ++j) {
+                    JSONObject materia = materias.getJSONObject(j);
+                    if (materia.getLong("idMateria") == examen.getMateria())
+                        examenNuevo.setCorrectorId(corrector.getLong("id"));
+                    else
+                        examenNuevo.setCorrectorId(-1L);
+                }
+            } else {
+                examenNuevo.setCorrectorId(-1L);
+            }
+        }
+        Long id = service.addExamen(examenNuevo);
+        
         URI uri = builder
                 .path("/examenes")
                 .path(String.format("/%d", id))

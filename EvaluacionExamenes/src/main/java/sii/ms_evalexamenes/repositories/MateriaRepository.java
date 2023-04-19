b621e36a536efddcb2cba64eeb0786d871b3f6e3
Jorge Rodríguez <jorgeroma2000@gmail.com>
2023-04-19 18:46:09 +0200

    Create Materia Repository

---
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/repositories/MateriaRepository.java
@@ -0,0 +1,8 @@
+package sii.ms_evalexamenes.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sii.ms_evalexamenes.entities.Materia;
+
+public interface MateriaRepository extends JpaRepository<Materia, Long> {
+}

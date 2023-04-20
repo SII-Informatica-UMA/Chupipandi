8995de1c353cbd2a77a59e6f1140c7305e598ac1
Marcos <marcos105ftr@gmail.com>
2023-04-20 09:44:07 +0200

    SavePoint

--- EvaluacionExamenes/src/test/java/sii/ms_evalexamenes/ExamenApplicationTest.java
+++ EvaluacionExamenes/src/test/java/sii/ms_evalexamenes/ExamenApplicationTest.java
@@ -26,6 +26,8 @@ import org.springframework.web.util.UriBuilderFactory;
 
 import sii.ms_evalexamenes.dtos.AsignacionDTO;
 import sii.ms_evalexamenes.dtos.ExamenDTO;
+import sii.ms_evalexamenes.entities.Examen;
+import sii.ms_evalexamenes.entities.Materia;
 import sii.ms_evalexamenes.repositories.ExamenRepository;
 
 
@@ -51,8 +53,7 @@ class ExamenApplicationTest {
 	@Nested
 	@DisplayName("cuando la base de datos está vacía")
 	public class BaseDatosVacia {
-        @Test
-		@DisplayName("devuelve error al acceder a un ingrediente concreto")
+        
 
         private URI uri(String scheme, String host, int port, String ...paths) {
             UriBuilderFactory ubf = new DefaultUriBuilderFactory();
@@ -109,6 +110,7 @@ class ExamenApplicationTest {
                 examenrepository.deleteAll();
             }
 
+            //get a examenes/{id}
             @Test
             @DisplayName("Devuelve 404 al acceder a un Examen Concreto cuando no hay Examenes")
             public void getExamenConcreto() {
@@ -117,15 +119,40 @@ class ExamenApplicationTest {
                 assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
             }
 
+            //get a examenes/{id} introduciendo un examen
+             
+            /* 
             @Test
-            @DisplayName("Inserta Correctamente un Examen cuando no hay Examenes")
-            public void getExamenConcreto1() {
+            @DisplayName("Devuelve 200 al acceder a un Examen Concreto Existente")
+            public void getExamenConcreto1() {    
+                Materia materia = new Materia();
+                materia.setId(1L);  
+                examenrepository.save(materia);     
+                Examen examen = new Examen();
+                examen.setId(1L);
+                examenrepository.save(examen);
+
+                var peticion = get("http", "localhost",port, "/examenes/1");
+                var respuesta = restTemplate.exchange(peticion,new ParameterizedTypeReference<ExamenDTO>() {});              
+                assertThat(respuesta.getStatusCode().is2xxSuccessful());
+            }
+            */
+            /*
+
+            //put a examenes/{id}
+            @Test
+            @DisplayName("Devuelve 404 al modifificar nota de un Examen cuando no hay Examenes")
+            public void getExamenConcreto2() {
+                examenrepository.deleteAll();
                 var examen = ExamenDTO.builder().materia(1L).build();
                 var peticion = put("http", "localhost",port, "/examenes/1",examen);
                 var respuesta = restTemplate.exchange(peticion,Void.class);
+                //assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
                 assertThat(respuesta.getStatusCode().is2xxSuccessful());  
             }
+            */
 
+            //get a examenes/asignacion
             @Test
             @DisplayName("Devuelve correctamente Lista al acceder a una Asignacion en concreto")
             public void getAsignaciones() {
@@ -143,6 +170,16 @@ class ExamenApplicationTest {
                 assertThat(respuesta.getBody().isEmpty());       
             }
         
+            
+            @Test
+            @DisplayName("Comprueba que la Lista de Asignacion este Vacia")
+            public void getAsignaciones2() {
+                var peticion = get("http", "localhost",port, "/examenes/asignacion");
+                var respuesta = restTemplate.exchange(peticion,new ParameterizedTypeReference <List<AsignacionDTO>>() {});                 
+                assertThat(respuesta.getBody().isEmpty());       
+            }
+
+
 
 
 

e8b467d97e8321560ffede2472bb5c7ddec26f1d
RocioLN15 <rlapeira@uma.es>
2023-04-21 11:08:17 +0200

    Tests 2

--- GestionCorrectores/src/test/java/sii/ms_corrector/CorrectorTests.java
+++ GestionCorrectores/src/test/java/sii/ms_corrector/CorrectorTests.java
@@ -6,13 +6,11 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +19,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.web.util.DefaultUriBuilderFactory;
@@ -48,6 +47,11 @@ class CorrectorTests {
     private MateriaEnConvocatoriaRepository matConvRepo;
     @Autowired
     private MateriaRepository matRepo;
+
+	@BeforeEach
+	public void initializeDatabase(){
+		correctorRepo.deleteAll();
+	}
 	
 	private URI uri(String scheme, String host, int port, String ...paths) {
 		UriBuilderFactory ubf = new DefaultUriBuilderFactory();
@@ -95,8 +99,6 @@ class CorrectorTests {
 		assertThat(actual.getIdUsuario()).isEqualTo(expected.getIdUsuario());
         assertThat(actual.getTelefono()).isEqualTo(expected.getTelefono());
         assertThat(actual.getMatEnConv()).isEqualTo(expected.getMatEnConv());
-		//System.out.println("\n actual " + actual.getMatEnConv());
-		//System.out.println("\nexpected " + expected.getMatEnConv());
         assertThat(actual.getMaximasCorrecciones()).isEqualTo(expected.getMaximasCorrecciones());
 	}
 	
@@ -115,11 +117,6 @@ class CorrectorTests {
 	@Nested
 	@DisplayName("Cuando la base de datos está vacía")
 	public class BaseDatosVacia {
-		@BeforeEach
-		public void eliminar(){
-			correctorRepo.deleteAll();
-		}
-
         // get un corrector por id
         @Test
         @DisplayName("acceder a un corrector que no existe")
@@ -129,7 +126,7 @@ class CorrectorTests {
                     new ParameterizedTypeReference<CorrectorDTO>() {});
 			
             assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
-			// comprobar que está vacía
+			assertThat(respuesta.getBody()).isNull();
         }
 
         // put un corrector por id
@@ -155,6 +152,7 @@ class CorrectorTests {
 			var respuesta = restTemplate.exchange(peticion,Void.class);
 
 			assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+			assertThat(respuesta.getBody()).isNull();
 		}
 
         // get todos los correctores sin especificar convocatoria (intentar)
@@ -173,7 +171,7 @@ class CorrectorTests {
 		// @Test
 		// @DisplayName("inserta correctamente un corrector")
 		// public void aniadirCorrector(){
-		// 	Preparamos el corrector a insertar
+		// 	// Preparamos el corrector a insertar
 		// 	var materia = MateriaDTO.builder().id(1L).nombre("Lengua").build();
 		// 	var corrector = CorrectorNuevoDTO.builder()
 		// 							.maximasCorrecciones(20)
@@ -187,13 +185,10 @@ class CorrectorTests {
 		// 				.idConvocatoria(1L).idMateria(1L).build());
 		// 	var corrExp = CorrectorDTO.fromCorrector(corrector.corrector());
 		// 	corrExp.setMaterias(materias);
-		// 	Preparamos la petición con el corrector dentro
+
 		// 	var peticion = post("http", "localhost", port, "/correctores", corrector);
-			
-		// 	Invocamos al servicio REST 
 		// 	var respuesta = restTemplate.exchange(peticion, Void.class);
 			
-		// 	Comprobamos el resultado
 		// 	assertThat(respuesta.getStatusCode().value()).isEqualTo(201);
 		// 	assertThat(respuesta.getHeaders().get("Location").get(0))
 		// 		.startsWith("http://localhost:"+port+"/correctores");
@@ -209,31 +204,33 @@ class CorrectorTests {
 
     @Nested
     @DisplayName("Cuando la base de datos tiene datos")
-	@TestInstance(Lifecycle.PER_CLASS)
     public class BaseDatosLLena{
-        @BeforeAll
-		public void anyadir(){
+        @BeforeEach
+		public void introduceDatos(){
 			Corrector c = new Corrector();
 			c.setIdUsuario(1L);
 			c.setMaximasCorrecciones(20);
 			c.setTelefono("123456789");
 
 			Materia materia = new Materia();
+			materia.setId(null);
 			materia.setIdMateria(1L);
 			materia.setNombre("Lengua");
 			matRepo.save(materia);
 
 			MateriaEnConvocatoria m = new MateriaEnConvocatoria();
+			m.setId(null);
+			m.setCorrector(c);
 			m.setIdConvocatoria(1L);
 			m.setMateria(materia);
-			
+			matConvRepo.save(m);
+
 			List<MateriaEnConvocatoria> materias = new ArrayList<>();
 			materias.add(m);
 			c.setMatEnConv(materias);
 
+			c.setId(null);
 			correctorRepo.save(c);
-			m.setCorrector(c);
-			matConvRepo.save(m);
 		}
 
 		// get todos los correctores sin especificar convocatoria (intentar)
@@ -245,14 +242,162 @@ class CorrectorTests {
 					new ParameterizedTypeReference<List<CorrectorDTO>>(){});
 			
 			assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
-			assertThat(respuesta.getBody()).hasSize((int)correctorRepo.count());
-			//assertThat((int)correctorRepo.count()).isEqualTo(1);
+			assertThat(respuesta.getBody()).hasSize(1);
 		}
 
-        // post un corrector
         // get un corrector por id
-        // delete un corrector
-        // put un corrector
+		@Test
+        @DisplayName("acceder a un corrector por id existente")
+        public void getCorrector(){
+            var peticion = get("http", "localhost", port, "/correctores/1");
+            var respuesta = restTemplate.exchange(peticion,
+                    new ParameterizedTypeReference<CorrectorDTO>() {});
+			
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+			assertThat(respuesta.getBody()).isNotNull();
+        }
 
+		@Test
+        @DisplayName("acceder a un corrector por id no existente")
+        public void getCorrNoExiste(){
+            var peticion = get("http", "localhost", port, "/correctores/24");
+            var respuesta = restTemplate.exchange(peticion,
+                    new ParameterizedTypeReference<CorrectorDTO>() {});
+			
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+			assertThat(respuesta.hasBody()).isFalse();
+        }
+		
+        // delete un corrector
+		@Test
+		@DisplayName("elimina un corrector cuando existe")
+		public void eliminarCorrector(){
+			var peticion = delete("http", "localhost",port, "/correctores/1");
+			var respuesta = restTemplate.exchange(peticion,Void.class);
+
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+			List<Corrector> corrDesp = correctorRepo.findAll();
+			//assertThat(corrDesp).hasSize(0);
+			assertThat(corrDesp).isEmpty();
+			assertThat(corrDesp).allMatch(c->c.getId()!=1);
+		}
+
+		@Test
+		@DisplayName("elimina un corrector cuando no existe")
+		public void elimCorrNoExiste(){
+			var peticion = delete("http", "localhost",port, "/correctores/24");
+			var respuesta = restTemplate.exchange(peticion,Void.class);
+
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+			assertThat(respuesta.hasBody()).isFalse();
+		}
+
+		/*
+        // put un corrector
+		@Test
+		@DisplayName("modifica un corrector cuando existe")
+		public void modificarCorrector(){
+			// crear nuevo corrector
+			Corrector corrector = new Corrector();
+			corrector.setId(null);
+			corrector.setMaximasCorrecciones(25);
+			corrector.setTelefono("123456789");
+
+			var peticion = put("http", "localhost", port, "/correctores/1", corrector);
+			var respuesta = restTemplate.exchange(peticion,Void.class);
+					
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+			Corrector corrBD = correctorRepo.findById(1L).get();
+			compruebaCampos(corrector, corrBD);
+			// comprobar materia y materiaEnConvocatoria
+		}
+
+		@Test
+		@DisplayName("modifica un corrector cuando no existe")
+		public void modCorrNoExiste(){
+			Corrector corrector = new Corrector();
+			corrector.setId(null);
+			corrector.setMaximasCorrecciones(25);
+			corrector.setTelefono("123456789");
+
+			var peticion = put("http", "localhost", port, "/correctores/24", corrector);
+			var respuesta = restTemplate.exchange(peticion,Void.class);
+					
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+			assertThat(respuesta.hasBody()).isFalse();
+		}
+
+		// post un corrector
+		@Test
+		@DisplayName("post corrector sin ID")
+		public void sinID() {
+			// crear corrector
+			Corrector c = new Corrector();
+			c.setId(null);
+			c.setIdUsuario(1L);
+			c.setMaximasCorrecciones(25);
+			c.setTelefono("112233445");
+
+			var peticion = post("http", "localhost", port, "/correctores", c);
+				
+			var respuesta = restTemplate.exchange(peticion, Void.class);
+				
+			compruebaRespuesta(c, respuesta);
+		}
+
+		@Test
+		@DisplayName("post corrector con ID no existente")
+		public void conIDNoEx() {
+			// crear corrector
+			Corrector c = new Corrector();
+			c.setId(3L);
+			c.setIdUsuario(3L);
+			c.setMaximasCorrecciones(25);
+			c.setTelefono("112233445");
+
+			var peticion = post("http", "localhost", port, "/correctores", c);
+				
+			var respuesta = restTemplate.exchange(peticion, Void.class);
+				
+			compruebaRespuesta(c, respuesta);
+		}
+
+		@Test
+		@DisplayName("post corrector con ID existente")
+		public void conIDEx() {
+			// crear corrector
+			Corrector c = new Corrector();
+			c.setId(1L);
+			c.setIdUsuario(1L);
+			c.setMaximasCorrecciones(25);
+			c.setTelefono("112233445");
+
+			var peticion = post("http", "localhost", port, "/correctores", c);
+			var respuesta = restTemplate.exchange(peticion, Void.class);
+			
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(409);
+			List<Corrector> correctores = correctorRepo.findAll();
+			assertThat(correctores).hasSize(1);
+		}
+
+		private void compruebaRespuesta(Corrector corrector, ResponseEntity<Void> respuesta) {
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(201);
+			assertThat(respuesta.getHeaders().get("Location").get(0))
+				.startsWith("http://localhost:"+port+"/correctores");
+			
+			List<Corrector> correctores = correctorRepo.findAll();
+			assertThat(correctores).hasSize(2);
+			
+			Corrector pepe = correctores.stream()
+					.filter(c->c.getTelefono().equals("112233445"))
+					.findAny()
+					.get();
+			
+			assertThat(respuesta.getHeaders().get("Location").get(0))
+				.endsWith("/"+pepe.getId());
+			compruebaCampos(corrector, pepe);
+			// comprobar materia y materiaEnConvocatoria
+		}
+		*/
     }
 }

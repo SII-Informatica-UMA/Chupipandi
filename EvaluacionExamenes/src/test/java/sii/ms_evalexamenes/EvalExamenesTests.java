44c29937d26314741e0d432957631a55998bb031
Jorge Rodríguez <jorgeroma2000@gmail.com>
2023-04-24 20:45:34 +0200

    Tests

--- EvaluacionExamenes/src/test/java/sii/ms_evalexamenes/EvalExamenesTests.java
+++ EvaluacionExamenes/src/test/java/sii/ms_evalexamenes/EvalExamenesTests.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.isNull;
 
 import java.net.URI;
 import java.net.URISyntaxException;

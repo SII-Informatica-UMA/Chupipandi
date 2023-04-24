d714a70a92d4b16de36e1e688fc7a086e5286dd8
MAngelGC <magc6102@uma.es>
2023-04-24 11:23:29 +0200

    Quitada anotación innecesaria @CrossOrigins

--- EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/controllers/NotasController.java
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/controllers/NotasController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

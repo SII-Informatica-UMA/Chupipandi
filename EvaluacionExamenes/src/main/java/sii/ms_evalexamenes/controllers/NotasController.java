d54f58e2368fbd42754a307bc5aa33d782443d57
MAngelGC <magc6102@uma.es>
2023-04-24 18:14:35 +0200

    Comprobación de DNI y apellido mediante endpoint fake de estudiantes

--- EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/controllers/NotasController.java
+++ EvaluacionExamenes/src/main/java/sii/ms_evalexamenes/controllers/NotasController.java
@@ -1,11 +1,16 @@
 package sii.ms_evalexamenes.controllers;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import sii.ms_evalexamenes.dtos.ExamenDTO;
 import sii.ms_evalexamenes.entities.Examen;
@@ -30,12 +42,41 @@ public class NotasController {
         this.service = service;
     }
 
+    private URI uri(String scheme, String host, int port, String ...paths) {
+        UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+        UriBuilder ub    = ubf.builder()
+                        .scheme(scheme)
+                        .host(host)
+                        .port(port);
+        for (String path : paths) {
+            ub = ub.path(path);
+        }
+        return ub.build();
+    }
+
+	private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.get(uri)
+			.accept(MediaType.APPLICATION_JSON)
+			.build();
+		return peticion;
+	}
+
     @GetMapping
-    public ResponseEntity<List<ExamenDTO>> getNotas(@RequestParam Long dni, @RequestParam String apellido) {
+    public ResponseEntity<List<ExamenDTO>> getNotas(@RequestParam String dni, @RequestParam String apellido) throws JsonMappingException, JsonProcessingException {
         
         //El objeto optional que nos devuelve el metodo getExamenByDniAndApellido nunca apararece como null por lo tanto no devuelve excepcion
-        //Siempre devuelve una lista 
-        Optional<List<Examen>> notas = service.getExamenByDniAndApellido(dni, apellido);
+        //Siempre devuelve una lista
+        var peticion = get("http", "localhost", 8080, "/estudiantes");
+
+        var respuesta = new RestTemplate().exchange(peticion, new ParameterizedTypeReference<List<Map<String, Object>>>() {});
+        if (respuesta.getBody().isEmpty())
+            return ResponseEntity.notFound().build();
+        Optional<Map<String, Object>> dniRespuesta = respuesta.getBody().stream().filter(est -> est.get("dni").equals(dni)).findFirst();
+        if (!(dniRespuesta.isPresent() && dniRespuesta.get().get("apellido1").equals(apellido)))
+            return ResponseEntity.notFound().build();
+        Long id = Integer.toUnsignedLong((Integer)(dniRespuesta.get().get("id")));
+        Optional<List<Examen>> notas = service.getExamenByDniAndApellido(id, apellido);
         
         // notas.isPresent siempre es igual a True 
         // Podemos Comprobar si la lista esta vacia
@@ -45,8 +86,9 @@ public class NotasController {
     
         List<ExamenDTO> lista = new ArrayList<>();
         for (Examen e : notas.get()){
-           lista.add(ExamenDTO.fromExamen(e));
+            lista.add(ExamenDTO.fromExamen(e));
         }
+        System.out.println(lista);
         return ResponseEntity.ok(lista);
     }
 

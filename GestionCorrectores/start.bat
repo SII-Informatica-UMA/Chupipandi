60b00e5ece24e1fd2d2bc01b539a8d671a2158f4
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2023-05-09 14:32:20 +0200

    Refactor start.bat - add skip test option

--- GestionCorrectores/start.bat
+++ GestionCorrectores/start.bat
@@ -1,18 +1,24 @@
-CALL mvn clean test
+echo Iniciando microservicio de gestion de correctores ...
 :: set /p variable="Modo de ejecucion (dev: BD en memroria, prod: BD persistente): "
 :: if "%variable%"=="prod" (
 ::     start cmd.exe @cmd /k "java -cp h2-2.1.210.jar org.h2.tools.Server -ifNotExists"
 :: )
 set bArg=false
 set prodArg=false
+set testArg=true
 for %%a in (%*) do (
     if "%%a"=="prod" set prodArg=true
     if "%%a"=="-b" set bArg=true
+    if "%%a"=="-skip" set testArg=false
 )
 
+if %testArg%==true (
+    CALL mvn test
+)
+
+:: start cmd.exe @cmd /k "java -cp h2-2.1.210.jar org.h2.tools.Server -ifNotExists"
 if %prodArg%==true (
     set variable=prod
-    start cmd.exe @cmd /k "java -cp h2-2.1.210.jar org.h2.tools.Server -ifNotExists"
 ) else (
     set variable=dev
 )

7f7a17b716744471220addee2e82afd0ddd81783
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2023-05-09 12:56:22 +0200

    Refactor runme.bat - better args

--- GestionCorrectores/runme.bat
+++ GestionCorrectores/runme.bat
@@ -3,13 +3,20 @@ CALL mvn clean test
 :: if "%variable%"=="prod" (
 ::     start cmd.exe @cmd /k "java -cp h2-2.1.210.jar org.h2.tools.Server -ifNotExists"
 :: )
-if "%1"=="prod" (
-    set variable=%1
+set bArg=false
+set prodArg=false
+for %%a in (%*) do (
+    if "%%a"=="prod" set prodArg=true
+    if "%%a"=="-b" set bArg=true
+)
+
+if %prodArg%==true (
+    set variable=prod
     start cmd.exe @cmd /k "java -cp h2-2.1.210.jar org.h2.tools.Server -ifNotExists"
 ) else (
     set variable=dev
 )
-if "%2"=="-b" (
+if %bArg%==true (
     :: Proceso en segundo plano
     CALL mvn spring-boot:start -Dspring-boot.run.arguments="--spring.profiles.active=%variable%"
 ) else (

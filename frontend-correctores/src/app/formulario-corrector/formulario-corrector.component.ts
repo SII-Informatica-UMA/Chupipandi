85ab18f1e04d94d6cfa48157227fbed1b79ae3d4
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2023-05-14 01:11:55 +0200

    Add form validation
    - Añadir corrector funciona
    
    - TODO: Editar corrector

--- frontend-correctores/src/app/formulario-corrector/formulario-corrector.component.ts
+++ frontend-correctores/src/app/formulario-corrector/formulario-corrector.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { CorrectorNuevo } from '../interfaces';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-corrector',
@@ -9,11 +9,51 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class FormularioCorrectorComponent {
   accion: "Añadir" | "Editar" | undefined;
-  corrector: CorrectorNuevo = {identificadorUsuario: BigInt(0), identificadorConvocatoria: undefined, telefono: '', materia: '', maximasCorrecciones: 0};
+  introducir: boolean = false;
 
-  constructor(public modal: NgbActiveModal) { }
+  public correctForm: FormGroup;
 
-  guardarCorrector(): void {
-    this.modal.close(this.corrector);
+  get identificadorUsuario() { return this.correctForm.get('identificadorUsuario'); }
+  get telefono() { return this.correctForm.get('telefono'); }
+  get maximasCorrecciones() { return this.correctForm.get('maximasCorrecciones'); }
+  get flexSwitchCheckDefault() { return this.correctForm.get('flexSwitchCheckDefault'); }
+  get identificadorConvocatoria() { return this.correctForm.get('identificadorConvocatoria'); }
+  get materia() { return this.correctForm.get('materia'); }
+  
+  constructor(public modal: NgbActiveModal) {
+    this.correctForm = this.createFormGroup();
   }
+
+  createFormGroup() {
+    return new FormGroup({
+      identificadorUsuario: new FormControl('', Validators.required),
+      telefono: new FormControl(''),
+      maximasCorrecciones: new FormControl('', Validators.required),
+      flexSwitchCheckDefault: new FormControl(''),
+      identificadorConvocatoria: new FormControl('', Validators.required),
+      materia : new FormControl('', Validators.required)
+    });
+  }
+
+  // Si usamos opcion de deshabilitar el boton de guardar hasta que el formulario sea valido
+  guardarCorrector(): void {
+    if (!this.correctForm.invalid) {
+      this.correctForm.removeControl('flexSwitchCheckDefault');
+      this.modal.close(this.correctForm.value);
+    }
+  }
+
+  // Si usamos opcion de mostrar error cada vez que se detecta un error en un campo
+  // guardarCorrector(): void {
+  //   if (!this.correctForm.invalid) {
+  //     this.correctForm.removeControl('flexSwitchCheckDefault');
+  //     this.modal.close(this.correctForm.value);
+  //   } else {
+  //     Object.keys(this.correctForm.controls).forEach(field => {
+  //         const control = this.correctForm.get(field);
+  //         control?.markAsTouched({ onlySelf: true });
+  //       }
+  //     );
+  //   }
+  // }
 }

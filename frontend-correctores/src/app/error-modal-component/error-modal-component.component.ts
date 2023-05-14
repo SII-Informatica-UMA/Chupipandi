07b57752a3367ec19074653a763580540eef2b32
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2023-05-14 03:39:46 +0200

    Refactor error-modal component

--- frontend-correctores/src/app/error-modal-component/error-modal-component.component.ts
+++ frontend-correctores/src/app/error-modal-component/error-modal-component.component.ts
@@ -3,11 +3,21 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-error-modal-component',
-  templateUrl: './error-modal-component.component.html',
+  template: `
+    <div class="modal-header">
+    <h4 class="modal-title">{{ errorCode }}</h4>
+    </div>
+    <div class="modal-body">
+      {{ errorMessage }}
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" (click)="modal.dismiss()">Cerrar</button>
+    </div>
+    `,
   styleUrls: ['./error-modal-component.component.css']
 })
 export class ErrorModalComponentComponent {
-  constructor(public modal: NgbActiveModal) {}
+  constructor(public modal: NgbActiveModal) { }
   errorCode: any;
   errorMessage: string = '';
 }

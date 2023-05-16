32480e1985ead089cd5b5ed7e4023e388395eff1
MAngelGC <magc6102@uma.es>
2023-05-16 14:03:09 +0200

    Actualizado frontend-evalexamenes

--- frontend-evalexamenes/src/app/formulario-notificacion/formulario-notificacion.component.ts
+++ frontend-evalexamenes/src/app/formulario-notificacion/formulario-notificacion.component.ts
@@ -1,8 +1,49 @@
-import { Time } from "@angular/common";
+import { TranslationWidth } from "@angular/common";
 import { Component, Injectable } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { NgbCalendar, NgbDateAdapter, NgbDateParserFormatter, NgbDatepickerModule, NgbDateStruct, NgbTimepicker, NgbTimepickerModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgbCalendar, NgbDate, NgbDateAdapter, NgbDateParserFormatter, NgbDatepickerI18n, NgbDatepickerModule, NgbDateStruct, NgbTimepickerModule } from "@ng-bootstrap/ng-bootstrap";
 import { Notificacion } from "../notificacion";
+
+const I18N_VALUES = {
+  es: {
+    weekdays: ['L', 'M', 'X', 'J', 'V', 'S', 'D'],
+    monthsFull: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    monthsShort:['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+    weekLabel: 'semana'
+  }
+};
+
+@Injectable()
+export class I18n {
+  language = 'es';
+}
+
+@Injectable()
+export class DatePickerI18n extends NgbDatepickerI18n {
+  constructor(private _i18n: I18n) {
+    super();
+  }
+
+  getWeekdayLabel(weekday: number): string {
+      return I18N_VALUES[this._i18n.language as keyof typeof I18N_VALUES].weekdays[weekday - 1];
+  }
+
+  override getWeekLabel(): string {
+      return I18N_VALUES[this._i18n.language as keyof typeof I18N_VALUES].weekLabel;
+  }
+
+  getMonthShortName(month: number): string {
+      return I18N_VALUES[this._i18n.language as keyof typeof I18N_VALUES].monthsShort[month - 1];
+  }
+
+  getMonthFullName(month: number): string {
+    return I18N_VALUES[this._i18n.language as keyof typeof I18N_VALUES].monthsFull[month - 1];
+  }
+
+  getDayAriaLabel(date: NgbDateStruct): string {
+      return `${date.day}/${date.month}/${date.year}`;
+  }
+}
 @Injectable()
 export class DateAdapter extends NgbDateAdapter<string> {
   readonly DELIMITER = '-';
@@ -51,7 +92,10 @@ export class DateParserFormatter extends NgbDateParserFormatter {
   templateUrl: './formulario-notificacion.component.html',
   styleUrls: ['./formulario-notificacion.component.css'],
   standalone: true,
-  imports: [NgbTimepickerModule, FormsModule, NgbDatepickerModule]
+  imports: [NgbTimepickerModule, FormsModule, NgbDatepickerModule],
+  providers: [I18n, {provide: NgbDatepickerI18n, useClass: DatePickerI18n},
+              DateAdapter, {provide: NgbDateAdapter, useClass: DateAdapter},
+              DateParserFormatter, {provide: NgbDateParserFormatter, useClass: DateParserFormatter}]
 })
 
 @Injectable({
@@ -59,16 +103,27 @@ export class DateParserFormatter extends NgbDateParserFormatter {
 })
 export class FormularioNotificacionComponent {
   notificacion: Notificacion = {plantillaAsunto: '', plantillaCuerpo: '', programacionEnvio: new Date, medios: []};
-  fecha?: NgbDateStruct;
-  hora: any = { hour: 13, minute: 30 };
+  hora: any = { hour: 0, minute: 0, second: 0};
   meridian: boolean = true;
 
-  model1?: string;
-  model2?: string;
+
+  model?: NgbDateStruct;
+  modelString?: string;
 
   constructor(private ngbCalendar: NgbCalendar, private dateAdapter: NgbDateAdapter<string>) {}
 
   get today() {
-    return this.dateAdapter.toModel(this.ngbCalendar.getToday())!;
+    return this.ngbCalendar.getToday();
   }
+
+  get todayString() {
+    return this.dateAdapter.toModel(this.ngbCalendar.getToday())!
+  }
+
+  get fechaYHora() {
+    let fecha = `${this.model?.year}-${this.model?.month}-${this.model?.day}T${this.hora.hour}:${this.hora.minute}:${this.hora.second}.000Z`;
+    return fecha;
+  }
+
+
 }

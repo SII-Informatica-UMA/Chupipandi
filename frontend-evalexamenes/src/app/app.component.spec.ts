2b862135e45d1caf58895ed8b2f27b1208244839
Jorge Rodríguez <jorgeroma2000@gmail.com>
2023-05-16 21:41:26 +0200

    Add app.component tests

--- frontend-evalexamenes/src/app/app.component.spec.ts
+++ frontend-evalexamenes/src/app/app.component.spec.ts
@@ -1,31 +1,62 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbAlertModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule, NgbAlertModule, NgbNavModule],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create the AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend-ms_evalexamenes'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-ms_evalexamenes');
-  });
+  // it(`should have as title 'frontend-ms_evalexamenes'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('frontend-ms_evalexamenes');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-ms_evalexamenes app is running!');
+    expect(compiled.querySelector('.row h2')?.textContent).toContain('Elige una opción');
+  });
+
+  it(`should call getNotas()`, () => {
+    let spy: any;
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    spy = spyOn(app, 'getNotas');
+    app.getNotas();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it(`should call restartNotificacion()`, () => {
+    let spy: any;
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    spy = spyOn(app, 'restartNotificacion');
+    app.restartNotificacion();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it(`should call sendNotificacion()`, () => {
+    let spy: any;
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    spy = spyOn(app, 'sendNotificacion');
+    app.sendNotificacion();
+    expect(spy).toHaveBeenCalled();
   });
 });

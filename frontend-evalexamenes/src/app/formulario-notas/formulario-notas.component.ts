1de514d00f6451731adaa2150e1f66ade5622a42
MAngelGC <magc6102@uma.es>
2023-05-18 09:55:06 +0200

    Actualizado formulario-notas

--- frontend-evalexamenes/src/app/formulario-notas/formulario-notas.component.ts
+++ frontend-evalexamenes/src/app/formulario-notas/formulario-notas.component.ts
@@ -1,9 +1,10 @@
-import { Component, Injectable } from "@angular/core";
+import { Component, EventEmitter, forwardRef, Injectable, Output } from "@angular/core";
+import { NG_VALUE_ACCESSOR } from "@angular/forms";
 import { Estudiante } from "../estudiante";
 @Component({
   selector: 'app-formulario-notas',
   templateUrl: './formulario-notas.component.html',
-  styleUrls: ['./formulario-notas.component.css']
+  styleUrls: ['./formulario-notas.component.css'],
 })
 
 @Injectable({
@@ -11,6 +12,7 @@ import { Estudiante } from "../estudiante";
 })
 export class FormularioNotasComponent {
   estudiante: Estudiante = {dni: '', apellido: ''};
+  @Output() getNotas: EventEmitter<Estudiante> = new EventEmitter<Estudiante>();
 
   constructor() { }
 }

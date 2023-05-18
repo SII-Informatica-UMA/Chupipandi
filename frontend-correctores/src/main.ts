8e589b412489ff248732f7266f0ef5be05e395c0
MAngelGC <magc6102@uma.es>
2023-05-18 23:56:16 +0200

    Arreglado front correctores

---
+++ frontend-correctores/src/main.ts
@@ -0,0 +1,10 @@
+/// <reference types="@angular/localize" />
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import '@angular/localize/init'
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
